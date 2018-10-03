158fffc5f22a21f4048fd44727794b21ebe7cfee
JeanJNAP <jeanjnap@gmail.com>
2018-10-03 14:36:59 -0300

    removendo funções desnecessárias

--- android/app/src/main/java/com/reactfolio/ActivityStarterModule.java
+++ android/app/src/main/java/com/reactfolio/ActivityStarterModule.java
@@ -1,28 +1,11 @@
 package com.reactfolio;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
 import android.widget.Toast;
 
-import com.facebook.react.ReactInstanceManager;
-import com.facebook.react.ReactNativeHost;
-import com.facebook.react.bridge.Callback;
-import com.facebook.react.bridge.CatalystInstance;
-import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.WritableNativeArray;
-import com.facebook.react.bridge.WritableNativeMap;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 
 /**
  * Expose Java to JavaScript.
@@ -41,10 +24,6 @@ class ActivityStarterModule extends ReactContextBaseJavaModule {
         eventEmitter = getReactApplicationContext().getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class);
     }
 
-    /**
-     * @return the name of this module. This will be the name used to {@code require()} this module
-     * from JavaScript.
-     */
     @Override
     public String getName() {
         return "ActivityStarter";
@@ -57,69 +36,5 @@ class ActivityStarterModule extends ReactContextBaseJavaModule {
 
     public void print(String texto){
         Toast.makeText(getReactApplicationContext(), texto, Toast.LENGTH_SHORT).show();
-    }
-
-    @Nullable
-    @Override
-    public Map<String, Object> getConstants() {
-        final Map<String, Object> constants = new HashMap<>();
-        constants.put("MyEventName", "MyEventValue");
-        return constants;
-    }
-
-    @ReactMethod
-    void navigateToExample() {
-        Activity activity = getCurrentActivity();
-
-    }
-
-    @ReactMethod
-    void dialNumber(@Nonnull String number) {
-        Activity activity = getCurrentActivity();
-        if (activity != null) {
-            Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + number));
-            activity.startActivity(intent);
-        }
-    }
-
-    @ReactMethod
-    void getActivityName(@Nonnull Callback callback) {
-        Activity activity = getCurrentActivity();
-        if (activity != null) {
-            callback.invoke(activity.getClass().getSimpleName());
-        }
-    }
-
-    @ReactMethod
-    void getActivityNameAsPromise(@Nonnull Promise promise) {
-        Activity activity = getCurrentActivity();
-        if (activity != null) {
-            promise.resolve(activity.getClass().getSimpleName());
-        }
-    }
-
-    @ReactMethod
-    void callJavaScript() {
-        Activity activity = getCurrentActivity();
-        if (activity != null) {
-            MainApplication application = (MainApplication) activity.getApplication();
-            ReactNativeHost reactNativeHost = application.getReactNativeHost();
-            ReactInstanceManager reactInstanceManager = reactNativeHost.getReactInstanceManager();
-            ReactContext reactContext = reactInstanceManager.getCurrentReactContext();
-
-            if (reactContext != null) {
-                CatalystInstance catalystInstance = reactContext.getCatalystInstance();
-                WritableNativeArray params = new WritableNativeArray();
-                params.pushString("Hello, JavaScript!");
-                catalystInstance.callFunction("JavaScriptVisibleToJava", "alert", params);
-            }
-        }
-    }
-
-    /**
-     * To pass an object instead of a simple string, create a {@link WritableNativeMap} and populate it.
-     */
-    static void triggerAlert(@Nonnull String message) {
-        eventEmitter.emit("MyEventValue", message);
     }
 }
